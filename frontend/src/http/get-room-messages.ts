92f55ca557477daffa785d547d104add31671d48
Samuel Morais030202 <moraissamuel009@gmail.com>
2025-03-19 11:19:50 -0300

    feat: ajustando listagem de mensagens de uma sala

--- frontend/src/http/get-room-messages.ts
+++ frontend/src/http/get-room-messages.ts
@@ -15,20 +15,20 @@ export async function getRoomMessages({ roomId }: GetRoomMessagesRequest): Promi
   const response = await fetch(`${import.meta.env.VITE_APP_API_URL}/rooms/${roomId}/messages`)
 
   const data: Array<{
-    id: string
-    room_id: string
-    message: string
-    reaction_count: number
-    answered: boolean
+    Id: string
+    RoomID: string
+    Message: string
+    CreationCount: number
+    Answered: boolean
   }> = await response.json()
 
   return {
     messages: data.map(item => {
       return {
-        id: item.id,
-        text: item.message,
-        amountOfReactions: item.reaction_count,
-        answered: item.answered,
+        id: item.Id,
+        text: item.Message,
+        amountOfReactions: item.CreationCount,
+        answered: item.Answered,
       }
     })
   }
